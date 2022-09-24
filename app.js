a1e6a92e166802113d96621781cda7f9247b81e5
Maria Danilova <Maru.Aruko@yandex.ru>
2022-09-25 00:50:08 +0400

    fix test

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Объект не найден' });
-});
+
 
 app.use((req, res, next) => {
   req.user = {
@@ -28,5 +26,8 @@ app.use((req, res, next) => {
 
 app.use('/users', routerUsers);
 app.use('/cards', routerCards);
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Объект не найден' });
+});
 
 app.listen(PORT);
