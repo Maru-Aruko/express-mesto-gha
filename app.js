45a6b6f3e4c638050c8bf0ce7d7c2e6eeb4cfbad
Maria Danilova <Maru.Aruko@yandex.ru>
2022-09-25 00:52:59 +0400

    fix test

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use((req, res, next) => {
   req.user = {
     _id: '632f119709646a5fb0d7b33b',
