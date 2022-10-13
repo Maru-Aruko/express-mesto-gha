d171910bf35af2955d83bfc1461fe20be15396cf
Maria Danilova <Maru.Aruko@yandex.ru>
2022-10-13 22:30:13 +0400

    fix cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,7 +5,7 @@ const ForbiddenError = require('../Errors/ForbiddenError');
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => res.send(cards))
     .catch(next);
 };
 
