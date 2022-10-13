5a5737559eedc7a2c14c5201c214a3154437c73f
Maria Danilova <Maru.Aruko@yandex.ru>
2022-10-13 04:01:18 +0400

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -12,11 +12,11 @@ module.exports = (req, res, next) => {
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
 
     if (method === 'OPTIONS') {
       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
       res.header('Access-Control-Allow-Headers', requestHeaders);
-      res.header('Access-Control-Allow-Credentials', true);
       res.end();
     }
   }
